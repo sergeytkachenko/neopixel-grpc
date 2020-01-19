9219a2d64d106c47fa5748ccf05a86379490ec86
sergey.tkachenko <serg.tkachenko@hotmail.com>
2020-01-19 20:20:14 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package neopixel_grpc
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os/exec"
+)
+
+const PYTHON_FILE_NAME = "neopixel-interface.py"
+
+func main() {
+	cmd := exec.Command("python", PYTHON_FILE_NAME)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		panic(err)
+	}
+	err = cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	go copyOutput(stdout)
+	go copyOutput(stderr)
+	cmd.Wait()
+}
+
+func copyOutput(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
---
+++ neopixel-interface.py
@@ -0,0 +1,35 @@
+import time
+import board
+import neopixel
+
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 16
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
+pixels.fill((255, 0, 0))
+# Uncomment this line if you have RGBW/GRBW NeoPixels
+# pixels.fill((255, 0, 0, 0))
+pixels.show()
+time.sleep(1)
+
+# Comment this line out if you have RGBW/GRBW NeoPixels
+pixels.fill((0, 255, 0))
+# Uncomment this line if you have RGBW/GRBW NeoPixels
+# pixels.fill((0, 255, 0, 0))
+pixels.show()
+time.sleep(1)
+
+# Comment this line out if you have RGBW/GRBW NeoPixels
+pixels.fill((0, 0, 255))
+pixels[15] = (0, 0, 0, 0.2)
+# Uncomment this line if you have RGBW/GRBW NeoPixels
+# pixels.fill((0, 0, 255, 0))
+pixels.show()
